4b1870a932ff4266eb3ca925a1fcdfc7496b0b24
hijiki51 <hibiki0719euph@gmail.com>
2021-02-24 02:14:45 +0900

    fix null or undefined

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,7 @@ const WEBHOOK_SECRET = getProp('WEBHOOK_SECRET')
 
 function doPost(e: GoogleAppsScript.Events.DoPost) {
   const params: AlertRequest = JSON.parse(e.postData.contents)
-  const discription =
-    params.message.length > 0 && params.message !== 'undefined'
-      ? `${params.message}\n`
-      : `\n`
+  const discription = params.message ? `${params.message}\n` : `\n`
   const text = `## [${params.title}](${params.ruleUrl})\n` + `${discription}`
   sendMessage(text)
 }
